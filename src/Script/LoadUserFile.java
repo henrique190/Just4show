da5178fd8bedad4ed171c4cb8eaa1bcc7d1ba891
henrique190 <43019035+henrique190@users.noreply.github.com>
2019-07-25 18:56:58 -0300

    ;

---
+++ src/Script/LoadUserFile.java
@@ -0,0 +1,105 @@
+package Script;
+
+
+import java.io.BufferedReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+
+public class LoadUserFile {
+
+	
+	public static ArrayList<String> user = new ArrayList<String>();
+	public static ArrayList<String> password = new ArrayList<String>();
+	public 	String[] grava;
+	public  String linha = "";
+	private Scanner ler;
+	
+	public LoadUserFile () {
+		try {
+			lerArquivo();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void lerArquivo() throws FileNotFoundException {
+
+		// System.out.printf("Informe o nome de arquivo texto:\n");
+		JFileChooser arquivo = new JFileChooser();
+		//arquivo.showOpenDialog(arquivo);
+
+		if (arquivo.showOpenDialog(arquivo) == JFileChooser.APPROVE_OPTION) {
+			
+
+			try {
+				ler = new Scanner(arquivo.getSelectedFile().toString());
+				String nome = ler.nextLine(); // ler primeira linha
+				System.out.printf("\not countain text file:\n");
+				FileReader arq = new FileReader(nome);
+				BufferedReader lerArq = new BufferedReader(arq);
+				
+				
+				
+				 
+				String line;
+				while ((line = lerArq.readLine()) != null) {
+					linha += ":" + line;
+					//line = lerArq.readLine();		
+					
+				}
+
+
+				arq.close();
+			
+				
+
+			} catch (IOException e) {
+				System.err.printf("Error Opening file: %s.\n", e.getMessage());
+			}
+
+		}
+		
+		
+		//System.out.println(linha);
+		grava = linha.split(":");
+/*		for(String temp : grava) {
+			System.out.println(temp);
+		}*/
+		
+		int a = 0;
+		int b = 1;
+		int i = 0;
+		do {
+			
+			user.add(grava[b]);
+			//proxy1[i] = grava[b];
+			//System.out.println(proxy1[i]);
+			i++;
+			b = b + 2;
+			a = a +2;
+		}while(b < grava.length);
+		
+			a = 0;
+			i = 0;
+		
+			do {
+			
+			password.add(grava[a]);
+			//port1[i] = grava[a];
+			//System.out.println(port1[i]);
+			i++;
+			a = a +2;
+		}while(a < grava.length);
+		
+		
+		
+	}
+
+}
--- src/Script/Test.java
+++ src/Script/Test.java
@@ -9,13 +9,19 @@ public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, MessagingException {
 		// TODO Auto-generated method stub
+		LoadUserFile file = new LoadUserFile();
 		
-		/*
-		 * CheckEmails checkEmails = new CheckEmails(); checkEmails.checkAction();
-		 * 
-		 * ChangePassword changePassword = new
-		 * ChangePassword(checkEmails.getUrlCatch()); changePassword.changelAction();
-		 */
+		int i = 0;
+		for (String x : file.user) {
+			System.out.println(file.user.get(i) + ":" + file.password.get(i+1));
+			i++;
+		}
+		
+		
+		
+		
+		
+
 	}
 
 }
