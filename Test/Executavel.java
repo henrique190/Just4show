f8cb5f1fec71e697401832300f6a5266e7dda0d9
henrique190 <43019035+henrique190@users.noreply.github.com>
2019-08-01 13:10:00 -0300

    .
    ;

---
+++ Test/Executavel.java
@@ -0,0 +1,13 @@
+
+public class Executavel {
+
+	public Executavel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t = new Test();
+	}
+
+}
